03326e3992b704eb532d7488ca593ad1643fbc98
Keval Shah <kevalshah93@gmail.com>
2014-03-20 11:38:39 -0700

    Added action bar

--- Multishare/src/com/example/multishare/MainActivity.java
+++ Multishare/src/com/example/multishare/MainActivity.java
@@ -1,25 +1,38 @@
 package com.example.multishare;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends Activity{
-
+	
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_layout);
-	}
-	
-	public void shareMessage(View view){
-		EditText editText1 = (EditText) findViewById(R.id.editText1);
-		String message = editText1.getText().toString();
-		EditText editText2 = (EditText) findViewById(R.id.editText2);
-		editText2.setText(message);
+		setupActionBar();
 	}
 	
 	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.menu, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@SuppressLint("NewApi")
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
 	
 }
