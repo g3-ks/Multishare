6a281be0daa685c4b8421db79dee33eb26b357ba
Keval Shah <kevalshah93@gmail.com>
2014-03-23 14:55:53 -0700

    Collapsed fragment class into add account activity. Displays user name when user logs in

--- Multishare/src/com/example/multishare/AddAccountActivity.java
+++ Multishare/src/com/example/multishare/AddAccountActivity.java
@@ -2,32 +2,61 @@ package com.example.multishare;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.content.pm.PackageManager.NameNotFoundException;
-import android.graphics.drawable.ColorDrawable;
+import android.content.pm.Signature;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.TextView;
 
 import com.facebook.Session;
 import com.facebook.SessionState;
 import com.facebook.UiLifecycleHelper;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.facebook.widget.ProfilePictureView;
 
-public class AddAccountActivity extends FragmentActivity {
+public class AddAccountActivity extends Activity {
 
-	private FacebookAccountFragment fragment;
+	private LoginButton authButton;
+	
+	private ProfilePictureView profile_picture;
+	
+	private TextView user_name;
+	
+	private GraphUser user;
+	
+	private UiLifecycleHelper uiHelper;
 		
 	private static final String TAG = "debug";
+	
+	private Session.StatusCallback callback = new Session.StatusCallback() {
+	    @Override
+	    public void call(Session session, SessionState state, Exception exception) {
+	        onSessionStateChange(session, state, exception);
+	    }
+	};
+	
+	private void onSessionStateChange(Session session, SessionState state, Exception exception) {
+	    if (state.isOpened()) {
+	        Log.i(TAG, "Logged in...");
+	    } else if (state.isClosed()) {
+	        Log.i(TAG, "Logged out...");
+	    }
+	}
+	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		uiHelper = new UiLifecycleHelper(this, callback);
+	    uiHelper.onCreate(savedInstanceState);
 		PackageInfo info;
 		try {
 		    info = getPackageManager().getPackageInfo("com.example.multishare", PackageManager.GET_SIGNATURES);
@@ -45,32 +74,74 @@ public class AddAccountActivity extends FragmentActivity {
 		    Log.e("no such an algorithm", e.toString());
 		} catch (Exception e) {
 		    Log.e("exception", e.toString());
+		    
+		    
 		}
-
 		
-		if (savedInstanceState == null) {
-	        // Add the fragment on initial activity setup
-	       fragment = new FacebookAccountFragment();
-	        getSupportFragmentManager()
-	        .beginTransaction()
-	        .add(android.R.id.content, fragment)
-	        .commit();
-	    } else {
-	        // Or set the fragment from restored state info
-	        fragment = (FacebookAccountFragment) getSupportFragmentManager()
-	        .findFragmentById(android.R.id.content);
+		setContentView(R.layout.add_account_layout);
+		
+		authButton = (LoginButton) findViewById(R.id.authButton);
+		//profile_picture = (ProfilePictureView) findViewById(R.id.profilePicture);		
+		
+		
+		user_name = (TextView)findViewById(R.id.user_name);	
+		authButton.setReadPermissions(Arrays.asList("email"));
+		authButton.setUserInfoChangedCallback(new LoginButton.UserInfoChangedCallback() {
+			@Override
+            public void onUserInfoFetched(GraphUser user) {
+                AddAccountActivity.this.user = user;
+                updateUI();
+            }
+        });
+	}
+	
+	 private void updateUI() {
+	        Session session = Session.getActiveSession();
+	       
+	        if (user != null) {
+	            user_name.setText(user.getName());
+	            //profile_picture.setProfileId(user.getId());
+	        } else {
+	            user_name.setText(null);
+	        }
 	    }
-		
-		
-		
+
+	
+	@Override
+	public void onResume() {
+	    super.onResume();
+	    Session session = Session.getActiveSession();
+	    if (session != null &&
+	           (session.isOpened() || session.isClosed()) ) {
+	        onSessionStateChange(session, session.getState(), null);
+	    }
+
+	    uiHelper.onResume();
 	}
 
 	@Override
-	protected void onActivityResult(int arg0, int arg1, Intent arg2) {
-		super.onActivityResult(arg0, arg1, arg2);
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    super.onActivityResult(requestCode, resultCode, data);
+	    uiHelper.onActivityResult(requestCode, resultCode, data);
 	}
-	
-	
+
+	@Override
+	public void onPause() {
+	    super.onPause();
+	    uiHelper.onPause();
+	}
+
+	@Override
+	public void onDestroy() {
+	    super.onDestroy();
+	    uiHelper.onDestroy();
+	}
+
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+	    super.onSaveInstanceState(outState);
+	    uiHelper.onSaveInstanceState(outState);
+	}
 	
 	
 	
